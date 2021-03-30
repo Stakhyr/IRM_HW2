a288e77c39ebf9e78ddcc45b1287c7ccd05176c2
Stakhyr <56436513+Stakhyr@users.noreply.github.com>
2021-03-30 17:25:54 +0300

    Add project files.

---
+++ IRM_HW2/FileCollectionProcessing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IRM_HW2
+{
+    class FileCollectionProcessing
+    {
+        public static void ProcessDirectory(string targetDirectory) 
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach(var test in fileEntries) 
+            {
+                Console.WriteLine(test);
+            }
+        }
+    }
+}
---
+++ IRM_HW2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IRM_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetDirectory = @"C:\Users\DS\Desktop\IRM\HW1\Books";
+            FileCollectionProcessing.ProcessDirectory(targetDirectory);
+        }
+    }
+}
